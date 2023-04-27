e4dc8348f3260b734c534970d73e15b5f7f06f36
Wanzhao Cheng <wc1609@nyu.edu>
2023-04-27 16:06:37 -0400

    modify admin page

--- src/animalShelter/AdminPage.java
+++ src/animalShelter/AdminPage.java
@@ -1,51 +1,79 @@
 package animalShelter;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AdminPage extends JFrame {
+    private JPanel mainPanel;
+
     public AdminPage() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setBackground(Color.white);
-        setupPanels();
-        setSize(1000,800);
-    }
+        setBounds(100, 100, 800, 550);
+        mainPanel = new JPanel();
+        mainPanel.setBackground(new Color(246, 246, 246));
+        mainPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-    public void setupPanels (){
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-        JLabel adminPage = new JLabel("Manage Animal Information");
-        adminPage.setFont(new Font("Serif", Font.PLAIN, 40));
-        adminPage.setLocation(400, 50);
-        adminPage.setForeground(new Color(42, 104, 206));
-        adminPage.setBackground(new Color(100, 20, 70));
+        setContentPane(mainPanel);
 
-        JButton home = new JButton("Homepage");
-        home.addActionListener(new ActionListener() {
-            @Override
+        JLabel lblNewLabel = new JLabel("Manage Information");
+        lblNewLabel.setForeground(Color.DARK_GRAY);
+        lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        lblNewLabel.setFont(new Font("Lava Kannada", Font.PLAIN, 40));
+
+        JButton logOutButton = new JButton("Log Out");
+        logOutButton.setForeground(Color.RED);
+        logOutButton.setFont(new Font("Lava Kannada", Font.PLAIN, 15));
+
+        JButton removeButton = new JButton("Remove Animal");
+        removeButton.setFont(new Font("Lava Kannada", Font.PLAIN, 18));
+        removeButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                new Main().setVisible(true);
-                dispose();
             }
         });
 
-//        JButton backButton = new JButton("Back").addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                new AdminPage().setVisible(true);
-//            }
-//        });
-        JButton addButton = new JButton("Add an Animal");
-        JButton removeButton = new JButton("Remove an Animal");
-        JButton updateButton = new JButton("Update information");
+        JButton updateButton = new JButton("Update Animal");
+        updateButton.setFont(new Font("Lava Kannada", Font.PLAIN, 18));
 
-        mainPanel.add(adminPage);
-        mainPanel.add(home);
-        mainPanel.add(addButton);
-        mainPanel.add(removeButton);
-        mainPanel.add(updateButton);
-        this.add(mainPanel);
+        JButton addButton = new JButton("Add Animal");
+        addButton.setBackground(Color.WHITE);
+        addButton.setFont(new Font("Lava Kannada", Font.PLAIN, 18));
+        GroupLayout gl_mainPanel = new GroupLayout(mainPanel);
+        gl_mainPanel.setHorizontalGroup(
+                gl_mainPanel.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(gl_mainPanel.createSequentialGroup()
+                                .addContainerGap(222, Short.MAX_VALUE)
+                                .addGroup(gl_mainPanel.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                        .addGroup(GroupLayout.Alignment.TRAILING, gl_mainPanel.createSequentialGroup()
+                                                .addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 422, GroupLayout.PREFERRED_SIZE)
+                                                .addGap(146))
+                                        .addGroup(GroupLayout.Alignment.TRAILING, gl_mainPanel.createSequentialGroup()
+                                                .addComponent(logOutButton, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE)
+                                                .addGap(318))
+                                        .addGroup(GroupLayout.Alignment.TRAILING, gl_mainPanel.createSequentialGroup()
+                                                .addGroup(gl_mainPanel.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                                        .addComponent(addButton, GroupLayout.PREFERRED_SIZE, 308, GroupLayout.PREFERRED_SIZE)
+                                                        .addComponent(removeButton, GroupLayout.PREFERRED_SIZE, 308, GroupLayout.PREFERRED_SIZE)
+                                                        .addComponent(updateButton, GroupLayout.PREFERRED_SIZE, 308, GroupLayout.PREFERRED_SIZE))
+                                                .addGap(226))))
+        );
+        gl_mainPanel.setVerticalGroup(
+                gl_mainPanel.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(gl_mainPanel.createSequentialGroup()
+                                .addGap(17)
+                                .addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+                                .addGap(33)
+                                .addComponent(addButton, GroupLayout.PREFERRED_SIZE, 73, GroupLayout.PREFERRED_SIZE)
+                                .addGap(34)
+                                .addComponent(removeButton, GroupLayout.PREFERRED_SIZE, 73, GroupLayout.PREFERRED_SIZE)
+                                .addGap(35)
+                                .addComponent(updateButton, GroupLayout.PREFERRED_SIZE, 73, GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 47, Short.MAX_VALUE)
+                                .addComponent(logOutButton, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
+                                .addGap(26))
+        );
+        mainPanel.setLayout(gl_mainPanel);
     }
 }
